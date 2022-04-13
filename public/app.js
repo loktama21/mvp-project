4674afe41450b09ac56fa2db2871511ad06c1383
Lok <lb.tamang77@gmail.com>
2022-04-13 12:27:19 -0600

    fetch method fix

--- public/app.js
+++ public/app.js
@@ -1,17 +1,31 @@
-const form = document.querySelector(".user-form");
-const displayToDo = document.querySelector(".display"); //display
+// const { response } = require("express")
+
+const form = document.querySelector(".user-form")
+const displayToDo = document.querySelector(".display") //display
+
+fetch("/list", {
+  method: "get",
+})
+  .then((res) => res.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      let newTodo = document.createElement("li")
+      newTodo.innerHTML = data[i].task_name
+      displayToDo.appendChild(newTodo)
+    }
+  })
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const data = new FormData(e.target);
+  e.preventDefault()
+  const data = new FormData(e.target)
   const todo = {
     task_name: data.get("task"),
     task_priority: data.get("priority"),
-  };
+  }
 
-  let newTodo = document.createElement("li");
-  newTodo.innerHTML = todo.task_name;
-  displayToDo.appendChild(newTodo);
+  let newTodo = document.createElement("li")
+  newTodo.innerHTML = todo.task_name
+  displayToDo.appendChild(newTodo)
 
   fetch("/todos", {
     method: "post",
@@ -21,31 +35,29 @@ form.addEventListener("submit", (e) => {
     },
   })
     .then((res) => {
-      return res.json();
+      return res.json()
     })
     .then((res) => {
-      console.log(res);
-    });
+      console.log(res)
+    })
+})
 
-  newTodo.addEventListener("dblclick", (e) => {
-    displayToDo.removeChild(newTodo);
-    e.preventDefault();
-    // const data = new FormData(e.target);
-    const todo = {
-      task_name: data.get("task"),
-      task_priority: data.get("priority"),
-    };
+document.getElementsByTagName("li").addEventListener("dblclick", (e) => {
+  displayToDo.removeChild(newTodo)
+  e.preventDefault()
+  // const data = new FormData(e.target);
+  const todo = {
+    task_name: data.get("task"),
+    task_priority: data.get("priority"),
+  }
 
-    //current working start
-    fetch("/todos", {
-      method: "delete",
-      body: JSON.stringify(todo),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    //current working end
-  });
-
-  console.log("Saved");
-});
+  //current working start
+  fetch("/todos", {
+    method: "delete",
+    body: JSON.stringify(todo),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  //current working end
+})
--- server.js
+++ server.js
@@ -1,67 +1,68 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const db = require("./db/conn");
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const db = require("./db/conn")
 
-// const { Pool } = require("pg");
+const { Pool } = require("pg")
 
-// const pool = new Pool({
-//   database: "todolist",
-// });
+const pool = new Pool({
+  database: "todolist",
+})
 
-// const PORT = 4000;
+// const PORT = process.env.PORT || 4000
 
-app.use(express.json());
-app.use(express.static("public"));
+app.use(express.json())
+app.use(express.static("public"))
 
 //get all tasks
-app.get("/todos", (req, res) => {
+app.get("/list", (req, res) => {
   pool
     .query("SELECT * FROM todos ORDER BY id;")
     .then((result) => {
-      res.send(result.rows);
+      console.log(result.rows)
+      res.send(result.rows)
     })
     .catch(() => {
-      res.sendStatus(503);
-    });
-});
+      res.sendStatus(503)
+    })
+})
 
 //post a task
 app.post("/todos", (req, res) => {
-  const { task_name, task_priority } = req.body;
+  const { task_name, task_priority } = req.body
   pool
     .query(
       "INSERT INTO todos(task_name, task_priority) VALUES($1,$2) RETURNING *;",
       [task_name, task_priority]
     )
     .then((result) => {
-      res.send(result.rows);
+      res.send(result.rows)
     })
     .catch(() => {
-      res.sendStatus(503);
-    });
-});
+      res.sendStatus(503)
+    })
+})
 
 //update a task
 app.patch("/todos/:id", (req, res) => {
-  const { task_name, task_priority } = req.body;
-  const { id } = req.params;
+  const { task_name, task_priority } = req.body
+  const { id } = req.params
   const query = `
     UPDATE todos SET 
     task_name = COALESCE($1, task_name),
     task_priority = COALESCE($2, task_priority)
     WHERE id=$3
     RETURNING *
-    `;
+    `
   pool
     .query(query, [task_name, task_priority, id])
     .then((result) => {
-      res.send(result.rows);
+      res.send(result.rows)
     })
     .catch(() => {
-      res.sendStatus(503);
-    });
-});
+      res.sendStatus(503)
+    })
+})
 
 //delete a task
 // app.delete("/todos/:id", (req, res) => {
@@ -78,20 +79,20 @@ app.patch("/todos/:id", (req, res) => {
 
 app.delete("/todos/:id", (req, res) => {
   // const { task_name, task_priority } = req.body;
-  const id = req.params.id;
+  const id = req.params.id
   pool
     .query(
       "DELETE FROM todos WHERE task_name=$1 AND task_priority = $2 RETURNING *;",
       [task_name, task_priority]
     )
     .then((result) => {
-      res.send(result.rows);
+      res.send(result.rows)
     })
     .catch(() => {
-      res.sendStatus(503);
-    });
-});
+      res.sendStatus(503)
+    })
+})
 
 app.listen(process.env.PORT, () => {
-  console.log("Listening to port: ", process.env.PORT);
-});
+  console.log("Listening to port: ", process.env.PORT)
+})
